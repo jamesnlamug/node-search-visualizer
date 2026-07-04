cc2bb625447fa047490c5366d5fb941ce469a515
jamesnlamug <jamesnlamug@gmail.com>
2026-07-04 16:04:41 -0700

    add dfs core algorithm

--- DFS.js
+++ DFS.js
@@ -1,3 +1,13 @@
+class DFSData {
+	constructor (searchedNodes, endNode) {
+		this.searchedNodes = searchedNodes;
+		this.endNode = endNode;
+		this.reachedEnd = false;
+
+		this.iterations = 0;
+	}
+}
+
 const dfsButton = document.querySelector("#dfs-button");
 dfsButton.addEventListener("click", runDFSAlgorithm);
 
@@ -15,25 +25,42 @@ function runDFSAlgorithm() {
 	endNode.element.classList.add("end-node");
 
 	// main loop
-	let iterations = 0; // failsafe for infinite loop
+	let data = runDFSStep(startNode, new DFSData([], endNode));
 
-	let searchedNodes = [];
-	let searchedNodeGroups = [];
-	let pathways = [];
-	let queue = [startNode];
-	let reachedEnd = false;
-
-	while (!reachedEnd && iterations < 10000) {
-
-		// end condition
-		if (currentNode === endNode) {
-			reachedEnd = true;
-		}
-	}
-
-	console.log(`[runDFSAlgorithm] DFS completed with ${iterations} iterations`);
+	console.log(`[runDFSAlgorithm] DFS completed with ${data.iterations} iterations`);
 
 	// run visuals
 	//let searchDelay = applySearchVisual(searchedNodeGroups);
 	//applyPathVisual(searchDelay, startNode, endNode, pathways);
+}
+
+function runDFSStep(currentNode, data) {
+	data.searchedNodes.push(currentNode);
+	data.iterations ++;
+
+	if (currentNode === data.endNode) {
+		data.reachedEnd = true;
+		return data;
+	}
+
+	if (data.iterations > 1000) {
+		console.log(`[runDFSStep] Reached max iteration count`);
+		return data;
+	}
+	
+	// get adjacent node in all directions
+	for (let direction of Direction.cardinalDirections) {
+		
+		let nextNode = getNodeInDirection(currentNode, direction);
+		if (nextNode != null && !data.searchedNodes.includes(nextNode)) {
+			
+			data = runDFSStep(nextNode, data);
+			if (data.reachedEnd) {
+				return data;
+			}
+		}
+	}
+
+	// reached max depth
+	return data;
 }
