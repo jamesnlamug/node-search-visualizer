9a7e4573e781de5e7fe9f12d81829222d778ffb1
jamesnlamug <jamesnlamug@gmail.com>
2026-07-04 16:30:33 -0700

    move visualization to main script

--- BFS.js
+++ BFS.js
@@ -85,36 +85,3 @@ function traceNodePathBackwards(node, paths) {
 	// did not find
 	return null;
 }
-
-const searchIterationDelayMilliseconds = 10;
-const pathIterationDelayMilliseconds = 20;
-function applySearchVisual(searchedNodeGroups) {
-	for (let group of searchedNodeGroups) {
-		for (let node of group.nodes) {
-
-			setTimeout(() => {
-				node.element.classList.add("searched-node");
-			}, searchIterationDelayMilliseconds*group.iteration);
-		}
-	}
-
-	return searchedNodeGroups[searchedNodeGroups.length-1].iteration*searchIterationDelayMilliseconds;
-}
-
-function applyPathVisual(searchDelay, startNode, endNode, paths) {
-
-	let pathedNode = traceNodePathBackwards(endNode, paths);
-
-	let pathNodes = [];
-	while (pathedNode != startNode) {
-		pathNodes.unshift(pathedNode);
-		pathedNode = traceNodePathBackwards(pathedNode, paths);
-	}
-
-	for (let i=0; i<pathNodes.length; i++) {
-		let node = pathNodes[i];
-		setTimeout(() => {
-				node.element.classList.add("path-node");
-		}, searchDelay + pathIterationDelayMilliseconds*i);
-	}
-}
